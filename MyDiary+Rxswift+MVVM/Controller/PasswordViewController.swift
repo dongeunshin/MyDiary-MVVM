17384a7756e246ecde82df26743f7b55e8746b14
Dong-eun Shin <7017410@naver.com>
2022-05-01 18:11:09 +0900

    update password setting features

---
+++ MyDiary+Rxswift+MVVM/Controller/PasswordViewController.swift
@@ -0,0 +1,50 @@
+//
+//  PasswordViewController.swift
+//  MyDiary+Rxswift+MVVM
+//
+//  Created by dong eun shin on 2022/05/01.
+//
+
+import UIKit
+import RealmSwift
+
+class PasswordViewController: UIViewController {
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBAction func passwordBttnAction(_ sender: Any) {
+
+        let newPW = passwordTextField.text!
+        if password.count == 0{
+            do{
+                try realm.write{ realm.add(Password(password: newPW)) }
+            } catch {
+                print("Error: \(error)")
+            }
+        }else {
+            do {
+                try realm.write {
+                    password.first?.password = newPW
+                }
+            } catch {
+                    print("Error")
+            }
+        }
+
+        passwordTextField.text = ""
+        let alert = UIAlertController(title: "비밀번호가 변경되었습니다.", message: "새로운 비밀번호 : \(password.first!.password)", preferredStyle: .alert)
+
+        let ok = UIAlertAction(title: "OK", style: .default) { _ in 
+            self.navigationController?.popViewController(animated: true)
+        }
+        alert.addAction(ok)
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    let realm = try! Realm()
+    lazy var password = self.realm.objects(Password.self)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
